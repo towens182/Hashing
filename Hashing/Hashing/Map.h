4ce21ea52c61b774e38a8023f80a4d3ee660016f
tyowe <tyowe@LAPTOP-EV8F1SAC>
2017-04-05 21:00:59 -0400

    Work

--- Hashing/Hashing/Map.h
+++ Hashing/Hashing/Map.h
@@ -6,7 +6,7 @@ public:
 	bool insert(string key, valueType value);	//adds a value to the map using the specified key
 	valueType get(string key);			//returns the value associated with the specified key
 	bool remove(string key);			//removes the value with the associated key
-	int size();					//returns number of elements stored in the map
+	int size();						//returns number of elements stored in the map
 };
 
 
