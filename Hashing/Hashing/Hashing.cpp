a112c93934af875df5286ae5623b37c8215cc6fc
tyowe <tyowe@LAPTOP-EV8F1SAC>
2017-04-09 17:29:56 -0400

    Lot of work completed. Need to work on get() function.

--- Hashing/Hashing/Hashing.cpp
+++ Hashing/Hashing/Hashing.cpp
@@ -1,32 +1,106 @@
 // Hashing.cpp : Defines the entry point for the console application.
 //
-
+#pragma once
 #include "stdafx.h"
 #include "Map.h"
 #include <iostream>
 #include <vector>
 #include <cstdlib>
 #include <string>
-using namespace std;
+#include <functional>
 
 int main()
 {
-	template <class valueType>
-	valueType T;
+	using namespace std;
 
-	
-	Map<int> myIntMap;
+	Map<int> * intMap = new Map<int>();
+	Map<double> * doubleMap = new Map<double>();
+	Map<string> * stringMap = new Map<string>();
+
+	cout << "Size of intMap: " << intMap->size() << endl;
+	cout << "Size of floatMap: " << doubleMap->size() << endl;
+	cout << "Size of stringMap: " << stringMap->size() << endl << endl;
+
+	cout << "Adding 100 elements to each map." << endl;
 
 
+	//Add elements to maps
 	for (int i = 1; i < 101; i++)
 	{
 		string mapInput =  "Key";
-		mapInput = mapInput + to_string(i);
-		
 
+		mapInput = mapInput + to_string(i);
+
+		intMap->insert(mapInput, i);
+		doubleMap->insert(mapInput, i + .5);
+		stringMap->insert(mapInput, to_string(i));
+	}
+
+	cout << "Size of intMap: " << intMap->size() << endl;
+	cout << "Size of doubleMap: " << doubleMap->size() << endl;
+	cout << "Size of stringMap: " << stringMap->size() << endl << endl;
+
+	cout << "Now removing various items" << endl;
+
+
+	if (intMap->remove("Key2") == true)
+	{
+		cout << "Removed Key2 from intMap" << endl;
+	}
+	else
+	{
+		cout << "Did not remove Key2 in intMap" << endl;
+	}
+
+	for (int i = 32; i < 36; i++)
+	{
+		if (doubleMap->remove("Key" + to_string(i)) == true)
+		{
+			cout << "Removed Key" << i << " from doubleMap" << endl;
+		}
+		else
+		{
+			cout << "Did not remove Key" << i << " from doubleMap" << endl;
+		}
+	}
+
+	if (stringMap->remove("Key101") == true)
+	{
+		cout << "Removed Key101 from stringMap" << endl;
+	}
+	else
+	{
+		cout << "Did not remove Key101 in stringMap" << endl;
+	}
+
+	cout << endl;
+	cout << "Size of intMap: " << intMap->size() << endl;
+	cout << "Size of doubleMap: " << doubleMap->size() << endl;
+	cout << "Size of stringMap: " << stringMap->size() << endl << endl;
+
+	cout << "Now attempting to add vaious elements." << endl;
+	if (intMap->insert("Key101", 101) == true)
+	{
+		cout << "Added Key101 to intMap" << endl;
+	}
+	if (intMap->insert("Key3", 3) == true)
+	{
+		cout << "Added Key3 to intMap" << endl;
+	}
+	if (intMap->insert("Key", 2) == true)
+	{
+		cout << "Added Key2 to intMap" << endl;
+	}
+	if(doubleMap->insert("Key101", 101.5) == true);
+	{
+		cout << "Added Key101 to doubleMap" << endl;
 	}
 
 
+	//Cleanup
+	delete intMap;
+	delete doubleMap;
+	delete stringMap;
 
     return 0;
 }
--- Hashing/Hashing/Map.h
+++ Hashing/Hashing/Map.h
@@ -1,5 +1,7 @@
 #pragma once
 #include <functional>
+#include <vector>
+#include <string>
 template <class valueType>
 class Map
 {
@@ -9,15 +11,93 @@ public:
 	Map<valueType>();
 	~Map<valueType>();
 
-	bool insert(string key, valueType value);	//adds a value to the map using the specified key
-	valueType get(string key);			//returns the value associated with the specified key
-	bool remove(string key);			//removes the value with the associated key
+	bool insert(std::string key, valueType value);	//adds a value to the map using the specified key
+	//valueType get(std::string key);			//returns the value associated with the specified key
+	bool remove(std::string key);			//removes the value with the associated key
 	int size();						//returns number of elements stored in the map
+	bool keyFound(std::string key);
 
 private:
-	vector<valueType> keyV;
-	vector<valueType> objV;
+	std::vector<std::size_t> keyV;
+	std::vector<valueType> objV;
 };
 
+template<class valueType>
+inline Map<valueType>::Map()
+{
+
+	//keyV = new std::vector<valueType>();
+	//objV = new std::vector<valueType>();
+}
+
+template <class valueType>
+inline Map<valueType>::~Map()
+{
+	/*delete keyV;
+	delete objV;*/
+}
 
 
+template<class valueType>
+inline bool Map<valueType>::insert(std::string key, valueType value)
+{
+	std::hash<std::string> hash_fn;
+
+	if (keyFound(key) == false)
+	{
+
+		keyV.push_back(hash_fn(key));
+		objV.push_back(value);
+
+		return true;
+	}
+	else
+	{
+		std::cout << "Entry already exists with that key!" << std::endl;
+		return false;
+	}
+}
+
+template<class valueType>
+inline bool Map<valueType>::remove(std::string key)
+{
+	std::hash<std::string> hash_fn;
+	int counter = 0;
+	for (std::vector<std::size_t>::iterator it = keyV.begin(); it != keyV.end(); ++it)
+	{
+
+		if (*it == hash_fn(key))
+		{
+			keyV.erase(keyV.begin()+counter);
+			objV.erase(objV.begin()+counter);
+
+			return true;
+		}
+
+		counter++;
+	}
+
+	return false;
+}
+
+template <class valueType>
+inline int Map<valueType>::size()
+{
+	return keyV.size();
+}
+
+template<class valueType>
+inline bool Map<valueType>::keyFound(std::string key)
+{
+	std::hash<std::string> hash_fn;
+
+	for (std::vector<std::size_t>::iterator it = keyV.begin(); it != keyV.end(); ++it)
+	{
+		if (*it == hash_fn(key))
+		{
+			return true;
+		}
+	}
+
+	return false;
+}
